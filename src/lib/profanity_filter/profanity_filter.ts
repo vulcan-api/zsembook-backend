761736fb4899c1b263ca7b93506e416d609b7a7b
Maksymilian Gala <mgwspinanie2006@wp.pl>
2023-03-26 17:09:54 +0200

    Add english to profanity filter

--- src/lib/profanity_filter/profanity_filter.ts
+++ src/lib/profanity_filter/profanity_filter.ts
@@ -2,7 +2,9 @@ import * as leoProfanity from 'leo-profanity';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import * as pl from './dictionaries/pl_PL.json';
+import * as en from './dictionaries/en_EN.json';
 leoProfanity.add(pl);
+leoProfanity.add(en);
 
 export function filterProfanity(text: string): string {
   return leoProfanity.clean(text);
