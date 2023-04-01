97ab91132fc24dfdd248111a3790a41eb5ea031a
Maksymilian Gala <mgwspinanie2006@wp.pl>
2023-04-01 22:50:11 +0200

    Order events by date

--- src/school-event/school-event.service.ts
+++ src/school-event/school-event.service.ts
@@ -4,12 +4,15 @@ import { DbService } from '../db/db.service';
 @Injectable()
 export class SchoolEventService {
   constructor(private readonly prisma: DbService) {}
+
   async getAllEvents(skip: number, take: number): Promise<any> {
     return await this.prisma.schoolEvent.findMany({
       skip,
       take,
+      orderBy: { date: 'desc' },
     });
   }
+
   async getEventById(eventId: number): Promise<object> {
     return await this.prisma.schoolEvent.findUniqueOrThrow({
       where: { id: eventId },
