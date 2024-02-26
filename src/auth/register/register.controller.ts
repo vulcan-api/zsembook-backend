5b5626bacabfb5b08807ffa8e3268c770660ec83
Maksymilian Gala <maksymilian.gala1@gmail.com>
2024-02-26 14:56:54 +0100

    fix

--- src/auth/register/register.controller.ts
+++ src/auth/register/register.controller.ts
@@ -1,14 +1,12 @@
-import { Controller, Post, Body, UseFilters } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { RegisterDto } from '../dto';
 import { AuthService } from '../auth.service';
-import { VulcanExceptionFilter } from '../exception/vulcanException.filter';
 
 @Controller('auth/register')
 export class RegisterController {
   constructor(private authService: AuthService) {}
 
   @Post()
-  @UseFilters(new VulcanExceptionFilter())
   async register(@Body() dto: RegisterDto) {
     await this.authService.signup(dto);
     return { msg: 'Successfully registered a new account!', statusCode: 201 };
